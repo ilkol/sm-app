461933cfbd753677879eff5eead314f7e5d4073f
ilkol <ilkolreserve@gmail.com>
2024-11-14 23:09:07 +0300

    date format

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -28,3 +28,49 @@ export function formatTimestampFromDB(time: number) {
 	const date = new Date(time * 1000);
 	return formatDate(date, timeFormat.date);
 }
+
+const TIMEZONE_MOSCOW = 3;
+
+export function formatGMT(time: Date, timezone = TIMEZONE_MOSCOW)
+{
+	updateTimeZone(time, timezone);
+
+	const
+		day = time.getDate(),
+		month = getMonthName(time.getMonth()),
+		year = time.getFullYear(),
+		hours = time.getHours(),
+		minutes = time.getMinutes(),
+		timeZoneStr = timeZoneString(timezone)
+	;
+	
+
+	return `${day}.${month}.${year}, ${hours}:${minutes} ${timeZoneStr}`;
+}
+function updateTimeZone(time: Date, timezone: number): void
+{
+	time.setSeconds(time.getSeconds() + 3600 * (timezone - TIMEZONE_MOSCOW));
+}
+
+function timeZoneString(timezone: number): string
+{
+	return `GMT${timezone > 0 ? ('+' + timezone) : timezone}`;
+}
+const monthNames = [
+	'янв.',
+    'февр.',
+    'марта',
+    'апр.', 
+    'мая',
+    'июня', 
+    'июля',
+    'авг.', 
+    'сент.',
+    'окт.', 
+    'нояб.',
+    'дек.'
+]
+function getMonthName(month: number): string
+{
+	return monthNames[month];
+}
